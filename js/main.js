a986693bf1acf886878f48578f6f1240e1e2502c
AlvinKeettikkal <alvinkeettikka123@gmail.com>
2024-10-02 22:37:23 -0600

    finished step 2

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@ let showResourcesButton = document.getElementById('show-resources');
 let resourcesDiv = document.querySelector('.javascript-resources');
 
 // Step 1
+
 showResourcesButton.addEventListener('click', () => {
   resourcesDiv.classList.remove('d-none');
 });
+
+// Step 2
+
+resourcesDiv.addEventListener('mouseover', (event) => {
+    console.log(event.target); 
+    let listItem = event.target.closest('.list-group-item');
+    if (listItem) {
+      listItem.classList.add('fw-bold');
+    }
+  });
