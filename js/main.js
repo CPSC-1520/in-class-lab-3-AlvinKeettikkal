169ec71c0af3c973e6f0a95b56b3d90ea3abaad9
AlvinKeettikkal <alvinkeettikka123@gmail.com>
2024-10-02 22:22:35 -0600

    finished Step 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Enter your code below.
+
+let showResourcesButton = document.getElementById('show-resources');
+let resourcesDiv = document.querySelector('.javascript-resources');
+
+// Step 1
+showResourcesButton.addEventListener('click', () => {
+  resourcesDiv.classList.remove('d-none');
+});
